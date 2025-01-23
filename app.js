bdfde2ba7f0770d60c06e651e30d75c111821407
Ali Rahmani <rahmaniran184@gmail.com>
2025-01-23 16:02:33 +0330

    'Apdate-Two'

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-function toggle() {
-    var toggleButton = document.getElementById('aside-toggle');
+// function toggle() {
+//     var toggleButton = document.getElementById('aside-toggle');
+//     toggleButton.classList.toggle("aside");
+// }
+
+// function toggleList(id, className) {
+//     var list = document.getElementById(id);
+//     list.classList.toggle(className);
+// }
+// function btntoggle() {
+//     toggleList("list", "show");
+// }
+
+// function btntoggle2() {
+//     toggleList("list2", "show2");
+
+// }
+
+const toggle = () => {
+    const toggleButton = document.querySelector('#aside-toggle');
     toggleButton.classList.toggle("aside");
-}
+};
 
-function toggleList(id, className) {
-    var list = document.getElementById(id);
+const toggleList = (id, className) => {
+    const list = document.querySelector(`#${id}`);
     list.classList.toggle(className);
-}
-function btntoggle() {
+};
+
+const btntoggle = () => {
     toggleList("list", "show");
-}
+};
 
-function btntoggle2() {
+const btntoggle2 = () => {
     toggleList("list2", "show2");
-
-}
+};
